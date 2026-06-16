767cfe8f65f3a21fb44cd88009d460e73c1e7ef1
Amalmj031 <amlgi12345678@gmail.com>
2026-06-16 10:45:12 +0530

    Update main.cpp

--- C++_PROJECTS/USER_DEFINDD_STRING/main.cpp
+++ C++_PROJECTS/USER_DEFINDD_STRING/main.cpp
@@ -1,1 +1,41 @@
-// main file
+#include"header.h"
+
+int main()
+{
+	STRING s;
+	STRING s1("amxixal");
+	STRING s2("MJ");
+	STRING s3;
+	
+	cout<<strlen1(s1)<<endl;
+	//cout<<strstr1(s2,"MJ")<<endl;
+	//cout<<strchr1(s1,'x')<<endl;
+	//cout<<strlwr1(s2)<<endl;
+	//cout<<strrev1(s1)<<endl;
+	//cout<<strncat1(s1,s2,2);
+	//if(strncmp1(s1,s2,2)==1)
+	//	cout<<"equal"<<endl;
+	//else
+	//	cout<<"notequal"<<endl;
+
+	//strncpy1(s3,s1,2);
+	//cout<<"strncpy1= "<<s3<<endl;
+	///////////////////////////////////////////////////////////////////////
+	//STRING s4=" great boy";
+	//STRING s5=s1+s4;
+	//cout<<s1<<endl<<"string="<<s2<<" s2(s3)= "<<s3<<" "<<endl;
+	//cout<<" s5=s1+s4 "<<" "<<s5<<endl;
+	//cin>>s;
+	//cout<<s<<" "<<endl;
+	//s1[0]='z';
+	//cout<<s1<<" "<<endl;
+	//if(s1==s4)
+	//	cout<<"equal"<<endl;
+	//else
+	//	cout<<"notequal"<<endl;
+	//STRING s6;
+	//strcpy1(s6,s4);
+	//cout<<s6<<" "<<endl;
+	
+	return 0;
+}
