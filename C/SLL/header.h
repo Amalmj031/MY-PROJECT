931121d67ba284e481aedde81401f9f8b3bf58fa
Amalmj031 <amlgi12345678@gmail.com>
2026-06-16 11:58:58 +0530

    Create header.h

---
+++ C/SLL/header.h
@@ -0,0 +1,12 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+extern void cmd_rmv(FILE *f1 , FILE *f2);
+extern void macro(FILE *f1 , FILE *f2);
+extern void header_include(FILE *tp1, FILE *tp2);
+
+#endif
